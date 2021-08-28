bd90b830c726a81c81c577e05460d674c12cfdae
Hassan Emad <>
2021-08-28 16:16:48 +0200

    TS added

---
+++ App.tsx
@@ -0,0 +1,24 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import type {Node} from 'react';
+import {
+  SafeAreaView,
+} from 'react-native';
+
+const App: () => Node = () => {
+
+  return (
+    <SafeAreaView>
+
+    </SafeAreaView>
+  );
+};
+
+export default App;
